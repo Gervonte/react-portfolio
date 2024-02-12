470ff7d3a11b5f1585854c6c568f34721ae50f4a
Gervonte Fowler <37189775+Gervonte@users.noreply.github.com>
2024-02-12 16:29:48 -0500

    Update about.jsx

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,136 +1,131 @@
-import React from "react";
-import myImage from "../img/myImage.png";
+import React from 'react';
+import myImage from '../img/myImage.png';
 
 class About extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      skills: [
-        { id: "HTML5_skill", content: "HTML5", porcentage: "80%", value: "80" },
-        { id: "CSS3_skill", content: "CSS3", porcentage: "75%", value: "75" },
-        {
-          id: "JavaScript_skill",
-          content: "JavaScript",
-          porcentage: "90%",
-          value: "90"
-        },
-        { id: "PHP_skill", content: "PHP", porcentage: "70%", value: "70" },
-        {
-          id: "ReactJS_skill",
-          content: "ReactJS",
-          porcentage: "80%",
-          value: "80"
-        },
-        {
-          id: "Python_skill",
-          content: "Python",
-          porcentage: "75%",
-          value: "75"
-        },
-        {
-          id: "VanillaJS_skill",
-          content: "VanillaJS",
-          porcentage: "85%",
-          value: "85"
-        },
-        {
-          id: "Wordpress_skill",
-          content: "Wordpress",
-          porcentage: "80%",
-          value: "80"
-        }
-      ],
-      about_me: [
-        {
-          id: "first-p-about",
-          content:
-            "Lorem ipsum pariatur consectetur laboris occaecat nulla aliqua irure ad deserunt duis. Eiusmod nulla cupidatat labore sint sit aute dolore irure nostrud ut incididunt. Anim laborum reprehenderit labore magna ut dolore quis irure. Labore ea duis deserunt ullamco irure fugiat deserunt ut nisi ea minim proident. Nisi consectetur do non magna duis aliqua minim minim veniam. In occaecat minim qui consequat elit mollit consectetur non id tempor. Amet adipisicing occaecat tempor culpa quis est duis."
-        },
-        {
-          id: "second-p-about",
-          content:
-            "Lorem ipsum pariatur consectetur laboris occaecat nulla aliqua irure ad deserunt duis. Eiusmod nulla cupidatat labore sint sit aute dolore irure nostrud ut incididunt. Anim laborum reprehenderit labore magna ut dolore quis irure. Labore ea duis deserunt ullamco irure fugiat deserunt ut nisi ea minim proident. Nisi consectetur do non magna duis aliqua minim minim veniam. In occaecat minim qui consequat elit mollit consectetur non id tempor. Amet adipisicing occaecat tempor culpa quis est duis."
-        },
-        {
-          id: "third-p-about",
-          content:
-            "Lorem ipsum pariatur consectetur laboris occaecat nulla aliqua irure ad deserunt duis. Eiusmod nulla cupidatat labore sint sit aute dolore irure nostrud ut incididunt. Anim laborum reprehenderit labore magna ut dolore quis irure. Labore ea duis deserunt ullamco irure fugiat deserunt ut nisi ea minim proident. Nisi consectetur do non magna duis aliqua minim minim veniam. In occaecat minim qui consequat elit mollit consectetur non id tempor. Amet adipisicing occaecat tempor culpa quis est duis."
-        }
-      ]
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			skills: [
+				{ id: 'HTML5_skill', content: 'HTML5', percentage: '80%', value: '80' },
+				{ id: 'CSS3_skill', content: 'CSS3', percentage: '75%', value: '75' },
+				{
+					id: 'JavaScript_skill',
+					content: 'JavaScript',
+					percentage: '90%',
+					value: '90'
+				},
+				{ id: 'PHP_skill', content: 'PHP', percentage: '70%', value: '70' },
+				{
+					id: 'ReactJS_skill',
+					content: 'ReactJS',
+					percentage: '80%',
+					value: '80'
+				},
+				{
+					id: 'Python_skill',
+					content: 'Python',
+					percentage: '75%',
+					value: '75'
+				},
+				{
+					id: 'VanillaJS_skill',
+					content: 'VanillaJS',
+					percentage: '85%',
+					value: '85'
+				},
+				{
+					id: 'Wordpress_skill',
+					content: 'Wordpress',
+					percentage: '80%',
+					value: '80'
+				}
+			],
+			about_me: [
+				{
+					id: 'first-p-about',
+					content:
+						'I am a Bahamian software engineer with a Bachelor’s of Science degree in Computer Science that I earned in 2020 from Florida Polytechnic University. Shortly after graduating I worked at a Series B Fintech Startup (Nova Credit) for ~2 years, initially remotely from Florida but I moved to San Francisco for a year to continue my work as a  Full Stack developer.  At Nova Credit I worked on the Credit Passport product that allows for international credit history to be used in the US and other destinations. During this time I contributed to the codebase primarily using Javascript/Typescript, Node, React, Express, Jest and Postgres. In particular my most recent work here involved converting the internal ‘Credit Passport’ API to a public API to allow for expansion in a multi-million dollar partnership with HSBC.'
+				},
+				{
+					id: 'second-p-about',
+					content:
+						'Since then I’ve returned to The Bahamas where I’ve primarily studied and honed my coding/ general software engineering skills daily through LeetCode (200+ solved), coding projects, Udemy courses and system design interview practice while looking for a new role.'
+				}
+			]
+		};
+	}
 
-  render() {
-    return (
-      <section id="about" className="about-mf sect-pt4 route">
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-12">
-              <div className="box-shadow-full">
-                <div className="row">
-                  <div className="col-md-6">
-                    <div className="row">
-                      <div
-                        className="col-sm-6 col-md-5"
-                        style={{ margin: "0 auto" }}
-                      >
-                        <div
-                          className="about-img"
-                          style={{ textAlign: "center" }}
-                        >
-                          <img
-                            className="img-fluid rounded b-shadow-a"
-                            alt=""
-                          />
-                        </div>
-                      </div>
-                    </div>
-                    <div className="skill-mf">
-                      {/* <p className="title-s">Skill</p> */}
-                      {this.state.skills.map(skill => {
-                        return (
-                          <React.Fragment key={skill.id}>
-                            <span>{skill.content}</span>{" "}
-                            <span className="pull-right">
-                              {skill.porcentage}
-                            </span>
-                            <div className="progress">
-                              <div
-                                className="progress-bar"
-                                role="progressbar"
-                                style={{ width: skill.porcentage }}
-                                aria-valuenow={skill.value}
-                                aria-valuemin="0"
-                                aria-valuemax="100"
-                              ></div>
-                            </div>
-                          </React.Fragment>
-                        );
-                      })}
-                    </div>
-                  </div>
-                  <div className="col-md-6">
-                    <div className="about-me pt-4 pt-md-0">
-                      <div className="title-box-2">
-                        <h5 className="title-left">About Me</h5>
-                      </div>
-                      {this.state.about_me.map(content => {
-                        return (
-                          <p className="lead" key={content.id}>
-                            {content.content}
-                          </p>
-                        );
-                      })}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    );
-  }
+	render() {
+		return (
+			<section id='about' className='about-mf sect-pt4 route'>
+				<div className='container'>
+					<div className='row'>
+						<div className='col-sm-12'>
+							<div className='box-shadow-full'>
+								<div className='row'>
+									<div className='col-md-6'>
+										<div className='row'>
+											<div
+												className='col-sm-6 col-md-5'
+												style={{ margin: '0 auto' }}
+											>
+												<div
+													className='about-img'
+													style={{ textAlign: 'center' }}
+												>
+													<img
+														className='img-fluid rounded b-shadow-a'
+														alt=''
+													/>
+												</div>
+											</div>
+										</div>
+										<div className='skill-mf'>
+											<p className='title-s'>Skill</p>
+											{this.state.skills.map(skill => {
+												return (
+													<React.Fragment key={skill.id}>
+														<span>{skill.content}</span>{' '}
+														<span className='pull-right'>
+															{skill.percentage}
+														</span>
+														<div className='progress'>
+															<div
+																className='progress-bar'
+																role='progressbar'
+																style={{ width: skill.percentage }}
+																aria-valuenow={skill.value}
+																aria-valuemin='0'
+																aria-valuemax='100'
+															></div>
+														</div>
+													</React.Fragment>
+												);
+											})}
+										</div>
+									</div>
+									<div className='col-md-6'>
+										<div className='about-me pt-4 pt-md-0'>
+											<div className='title-box-2'>
+												<h5 className='title-left'>About Me</h5>
+											</div>
+											{this.state.about_me.map(content => {
+												return (
+													<p className='lead' key={content.id}>
+														{content.content}
+													</p>
+												);
+											})}
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		);
+	}
 }
 
 export default About;
